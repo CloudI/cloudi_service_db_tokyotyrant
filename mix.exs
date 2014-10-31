e58331636dbd4bdbc8e1d2a78c1a359948232af3
Michael Truog <mjtruog@gmail.com>
2014-10-30 18:33:49 -0700

    Update hex package.

---
+++ mix.exs
@@ -0,0 +1,32 @@
+defmodule CloudIServiceDbTokyotyrant do
+  use Mix.Project
+
+  def project do
+    [app: :cloudi_service_db_tokyotyrant,
+     version: "1.4.0-rc.4",
+     language: :erlang,
+     description: description,
+     package: package,
+     deps: deps]
+  end
+
+  defp deps do
+    [{:etokyotyrant,
+      [git: "https://github.com/okeuday/etokyotyrant.git",
+       branch: "master"]},
+     {:cloudi_core, "~> 1.4.0-rc.4"}]
+  end
+
+  defp description do
+    "Erlang/Elixir Cloud Framework Tokyo Tyrant Service"
+  end
+
+  defp package do
+    [files: ~w(src doc rebar.config README.markdown),
+     contributors: ["Michael Truog"],
+     licenses: ["BSD"],
+     links: %{"Website" => "http://cloudi.org",
+              "GitHub" => "https://github.com/CloudI/" <>
+                          "cloudi_service_db_tokyotyrant"}]
+   end
+end
